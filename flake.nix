5a674137d38abd08d60165a7a5f32976a1723671
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-07 04:49:03 -0400

    change flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "nebu's nix config";
+  description = "nebu's NixOs configs";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
