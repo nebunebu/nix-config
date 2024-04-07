63e5ad6b0f11a4ea19ae69439a96a7685b8445b5
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-07 08:43:26 -0400

    switch to systemd boot

--- hosts/t5610/configuration.nix
+++ hosts/t5610/configuration.nix
@@ -36,16 +36,21 @@
 
   environment.sessionVariables = {
     HOSTNAME = "t5610";
+    Editor = "nvim";
   };
 
   networking.hostName = "t5610";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
+  # boot.loader.grub.enable = true;
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "nodev"; # or "nodev" for efi only
 
 
   console.useXkbConfig = true;
