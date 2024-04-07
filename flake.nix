6ce8b9699b0da49805c205c3b3237fe9aa8c3d50
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-07 02:49:49 -0400

    add unstablePkgs

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "nebu's nix config";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,26 +15,38 @@
     };
     nix-colors.url = "github:misterio77/nix-colors";
   };
-  outputs = { nixpkgs, ... } @inputs:
+  outputs = { nixpkgs, nixpkgs-unstable, ... } @inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      unstablePkgs = nixpkgs-unstable.legacyPackages.${system};
+    in
     {
-      #test
       nixosConfigurations = {
         t5610 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
+          inherit system;
+          specialArgs = { inherit inputs pkgs unstablePkgs; };
           modules = [
             ./hosts/t5610/configuration.nix
             inputs.disko.nixosModules.default
             (import ./hosts/t5610/disko.nix { device = "/dev/sda"; })
+            {
+              home-manager.users.nebu = import ./hosts/t5610/home.nix;
+              home-manager.extraSpecialArgs = { inherit inputs pkgs unstablePkgs; };
+            }
           ];
         };
         x230t = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
+          inherit system;
+          specialArgs = { inherit inputs pkgs unstablePkgs; };
           modules = [
-            ./hosts/x230t/configuration.nix
+            ./hosts/t5610/configuration.nix
             inputs.disko.nixosModules.default
             (import ./hosts/x230t/disko.nix { device = "/dev/sda"; })
+            {
+              home-manager.users.nebu = import ./hosts/x230t/home.nix;
+              home-manager.extraSpecialArgs = { inherit inputs pkgs unstablePkgs; };
+            }
           ];
         };
       };
