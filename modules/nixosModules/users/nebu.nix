3fd11508565b98eaa0898056104551ab6ced19a2
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-08 06:32:05 -0400

    remove dead code

---
+++ modules/nixosModules/users/nebu.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nebu = {
+    isNormalUser = true;
+    initialPassword = "password";
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+      git
+      waybar
+    ];
+  };
+
+}
