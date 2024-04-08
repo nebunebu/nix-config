ba77f020d13f5182d9bf4608a70df7039ca12fb4
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-08 06:33:13 -0400

    create users module

--- hosts/x230t/configuration.nix
+++ hosts/x230t/configuration.nix
@@ -85,18 +85,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nebu = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-      git
-      waybar
-    ];
-  };
-
   programs.zsh.enable = true;
 
   # List packages installed in system profile. To search, run:
