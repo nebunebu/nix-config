14f547d555c9ac6182764ee6c3e1537915d697f7
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-08 07:25:53 -0400

    move nix settings to nixosModules

---
+++ modules/nixosModules/nix/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command flakes" ];
+      auto-optimise-store = true;
+      allowed-users = [ "@wheel" ];
+      trusted-users = [ "@wheel" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    nixPath = [
+      "nixpkgs=${pkgs.path}"
+    ];
+  };
+  nixpkgs.config.allowUnfree = true;
+}
