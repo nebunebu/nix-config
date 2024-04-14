4bb0bc7671cab63963b21665a54519fbb4330a4f
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-14 14:40:04 -0400

    add app.hyprland.x230t, app.hyprland.t5610

--- hosts/t5610/home.nix
+++ hosts/t5610/home.nix
@@ -12,7 +12,12 @@
   app.git.enable = true;
   app.kitty.enable = true;
   app.sioyek.enable = true;
-  app.hyprland.enable = true;
+
+  app.hyprland = {
+    enable = true;
+    t5610.enable = true;
+  };
+
   app.tmux.enable = true;
   app.zsh.enable = true;
   app.theme.enable = true;
