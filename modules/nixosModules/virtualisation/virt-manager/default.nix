8429d5fdc0e21d98197dcccbc44e7b40621f3ca7
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-15 23:29:56 -0400

    add virt-manager (broken)

--- modules/nixosModules/virtualisation/virt-manager/default.nix
+++ modules/nixosModules/virtualisation/virt-manager/default.nix
@@ -6,13 +6,45 @@
   };
 
   config = lib.mkIf config.app.virt-manager.enable {
-    users.users.nebu.extraGroups = [ "libvirtd" ];
+    virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
-    services.spice-vdagentd.enable = true;
-    networking.firewall.trustedInterfaces = [ "virbr0" ];
-    virtualisation = {
-      libvirtd.enable = true;
-      libvirtd.onBoot = "start";
+    users.users.nebu.extraGroups = [ "libvirtd" ];
+
+    home-manager.users.nebu.dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
     };
   };
+
+  #   programs.virt-manager.enable = true;
+  #   services.spice-vdagentd.enable = true;
+  #   networking.firewall.trustedInterfaces = [ "virbr0" ];
+  #   programs.dconf.enable = true;
+  #   virtualisation = {
+  #     libvirtd = {
+  #       enable = true;
+  #       onBoot = "start";
+  #       qemu = {
+  #         ovmf.enable = true;
+  #         swtpm.enable = true;
+  #       };
+  #
+  #
+  #       # extraConfig = ''
+  #       #   <network>
+  #       #     <name>default</name>
+  #       #     <bridge name="virbr0"/>
+  #       #     <forward mode="nat"/>
+  #       #     <ip address="192.168.122.1" netmask="255.255.255.0">
+  #       #       <dhcp>
+  #       #         <range start="192.168.122.2" end="192.168.122.254"/>
+  #       #       </dhcp>
+  #       #     </ip>
+  #       #   </network>
+  #       # '';
+  #     };
+  #   };
+  # };
 }
