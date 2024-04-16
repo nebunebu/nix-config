f53bae9ba1d07ce0e859b02203332fafe6ae3181
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-16 17:21:59 -0400

    delete dead code

--- hosts/t5610/configuration.nix
+++ hosts/t5610/configuration.nix
@@ -10,7 +10,6 @@
     ];
 
   nixpkgs.config.allowUnfree = true;
-  # nixpkgs.
 
   # boot.initrd.services.udev.rules = ''
   #   ATTRS{idVendor}=="047f", ATTRS{idProduct}=="c056", ENV{ID_MODEL}="Plantronics Blackwire C3220", ENV{ID_MODEL_FROM_DATABASE}="Plantronics Blackwire C3220", ENV{ID_VENDOR}="Plantronics", ENV{ID_VENDOR_FROM_DATABASE}="Plantronics", NAME="Plantronics Blackwire C3220"
