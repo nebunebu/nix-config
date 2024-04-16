ba135635c82c12ffdac4cf57d63627042d354151
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-15 21:45:13 -0400

    add vesktop, discord

--- modules/nixosModules/discord/default.nix
+++ modules/nixosModules/discord/default.nix
@@ -1,18 +1,17 @@
-{ options
-, config
-, lib
-, pkgs
-, ...
-}:
+{ config, lib, pkgs, ... }:
 
 {
   options = {
-    discord.enable = lib.mkEnableOption "enable discord";
+    app.discord.enable = lib.mkEnableOption "enable discord";
   };
 
 
-  config = lib.mkIf config.discord.enable {
-    nixpkgs.config.allowUnfree = true;
+  config = lib.mkIf config.app.discord.enable {
+    # nixpkgs.config.allowUnfree = true;
+
+    # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    #   "discord"
+    # ];
     environment.systemPackages = [
       (pkgs.discord.override {
         withOpenASAR = true;
