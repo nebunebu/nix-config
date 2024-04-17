ef73056d30d83afb94c84af95a1ae53750f40a22
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-17 16:20:44 -0400

    add homeManagerModules/cli-tools.nix

---
+++ modules/homeManagerModules/cli-tools.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  home.packages = builtins.attrValues {
+    inherit (pkgs)
+      nerdfix
+      usbutils
+      tree
+      nitch
+      ripgrep-all
+      jq
+      pup
+      # fd
+      # bottom
+      # ripgrep
+      # ffmpeg
+      # yt-dlp
+      # distrobox
+      # lazygit
+      # libnotify
+      # invidtui
+      ;
+  };
+}
