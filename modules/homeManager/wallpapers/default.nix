4b6ee985dd05c31d63ef724127613be7a50c2431
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-18 01:07:12 -0400

    fix wallpaper

--- modules/homeManager/wallpapers/default.nix
+++ modules/homeManager/wallpapers/default.nix
@@ -1,7 +1,7 @@
 {
   imports = [
-    ./wallhaven-d6vm2l.jpg
-    ./wallhaven-exvprr.jpg
-    ./wallhaven-qzpv5q.jpg
+    # ./wallhaven-d6vm2l.jpg
+    # ./wallhaven-exvprr.jpg
+    # ./wallhaven-qzpv5q.jpg
   ];
 }
