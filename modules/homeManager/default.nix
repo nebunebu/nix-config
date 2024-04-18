5e4078318f37362243e02c4f82fb9afd653b53ec
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-17 21:52:06 -0400

    seperate homeManager commonalities

--- modules/homeManager/default.nix
+++ modules/homeManager/default.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
+    inputs.nix-colors.homeManagerModules.default
     ./git
     ./kitty
     ./hyprland
@@ -16,12 +17,41 @@
     ./swww
     ./nix-tools.nix
     ./cli-tools.nix
-    # ./wallpapers
   ];
 
-  home.packages = builtins.attrValues {
-    inherit (pkgs)
+  colorScheme = inputs.nix-colors.colorSchemes.rose-pine;
+
+  home = {
+    username = "nebu";
+    homeDirectory = "/home/nebu";
+    packages = with pkgs; [
       python3
-      ;
+      firefox
+      pavucontrol
+      # krita
+      # gimp
+      # thunderbird
+      # fd
+      # bottom
+      # ffmpeg
+      # distrobox
+      # lazygit
+      # libnotify
+      # invidtui
+      # noto-fonts
+      (nerdfonts.override {
+        fonts = [
+          "DroidSansMono"
+          "JetBrainsMono"
+          "HeavyData"
+        ];
+      })
+    ];
   };
+
+  programs.home-manager.enable = true;
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
+  systemd.user.startServices = "sd-switch";
+  home.stateVersion = "23.11";
 }
