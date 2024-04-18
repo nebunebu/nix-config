8eec89ade003a9ee74346f95dbf4a9e1ce2721c4
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-17 23:45:34 -0400

    fix x230t errors

--- hosts/x230t/nixOS/default.nix
+++ hosts/x230t/nixOS/default.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/nixosModules
+    ../../../modules/nixOS
     inputs.home-manager.nixosModules.default
     inputs.niri.nixosModules.niri
   ];
