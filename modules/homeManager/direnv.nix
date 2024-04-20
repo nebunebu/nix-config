48923857b8cb44147afdbba3a3f33c49c08b8198
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-20 14:59:43 -0400

    remove zsh flakify function

--- modules/homeManager/direnv.nix
+++ modules/homeManager/direnv.nix
@@ -4,4 +4,16 @@
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
+
+  # programs.zsh.initExtra = /* bash */ ''
+  #   flakify() {
+  #     if [ ! -e flake.nix ]; then
+  #       nix flake new -t github:nix-community/nix-direnv .
+  #     elif [ ! -e .envrc ]; then
+  #       echo "use flake" > .envrc
+  #       direnv allow
+  #     fi
+  #     ${EDITOR:-vim} flake.nix
+  #   }
+  # '';
 }
