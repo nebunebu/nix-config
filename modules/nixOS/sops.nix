d47ee0bb7c0fa23164b97d41e2c1d9d97f0c3c55
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-20 07:46:11 -0400

    set user password with sops (broken)

--- modules/nixOS/sops.nix
+++ modules/nixOS/sops.nix
@@ -8,7 +8,13 @@
   environment.systemPackages = [ pkgs.sops ];
 
   sops = {
-    defaultSopsFile = ../../../secrets/secrets.yaml;
+    defaultSopsFile = ../../secrets/secrets.yaml;
     age.keyFile = "/home/nebu/.config/sops/age/keys.txt";
+    secrets.nebu-password.neededForUsers = true;
+  };
+
+  users.users.nebu = {
+    isNormalUser = true;
+    hashedPasswordFile = config.sops.secrets.nebu-password.path;
   };
 }
