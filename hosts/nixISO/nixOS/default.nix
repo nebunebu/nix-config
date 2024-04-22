73154af2fc2e7726abd4f6e59297b3beb9b2170b
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-21 20:47:23 -0400

    swap caps and escape on nixISO

--- hosts/nixISO/nixOS/default.nix
+++ hosts/nixISO/nixOS/default.nix
@@ -16,5 +16,21 @@
     pkgs.ripgrep-all
   ];
 
+  # TODO: isolate into module
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "US/Eastern";
+  services = {
+    openssh.enable = true;
+    xserver = {
+      layout = "us";
+      xkbOptions = "caps:swapescape";
+    };
+  };
+
+  console = {
+    useXkbConfig = true;
+    font = "Lat2-Terminus16";
+    # keyMap = "us";
+  };
   system.stateVersion = "23.11";
 }
