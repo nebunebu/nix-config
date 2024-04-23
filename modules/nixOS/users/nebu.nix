84d1887b2008aed6abc320d08e7b7bb1eba49636
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-22 23:37:43 -0400

    used hashedPasswordFile

--- modules/nixOS/users/nebu.nix
+++ modules/nixOS/users/nebu.nix
@@ -4,10 +4,13 @@
 
   sops.secrets.nebu-password.neededForUsers = true;
 
-  users.users.nebu = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    # hashedPasswordFile = config.sops.secrets.test-password.path;
+  users = {
+    mutableUsers = false;
+    users.nebu = {
+      isNormalUser = true;
+      shell = pkgs.zsh;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      hashedPasswordFile = config.sops.secrets.nebu-password.path;
+    };
   };
 }
