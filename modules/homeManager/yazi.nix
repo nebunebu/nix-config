ddfb3bcb71531b777daa7b0cac092fb7bbc7deb8
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-28 19:27:59 -0400

    add yazi

---
+++ modules/homeManager/yazi.nix
@@ -0,0 +1,18 @@
+{
+  programs.yazi = {
+    enable = true;
+    enableZshIntegration = true;
+    # keymap = {};
+    # settings = {};
+    theme = {
+      filetype = {
+        rules = [
+          { fg = "#7AD9E5"; mime = "image/*"; }
+          { fg = "#F3D398"; mime = "video/*"; }
+          { fg = "#F3D398"; mime = "audio/*"; }
+          { fg = "#CD9EFC"; mime = "application/x-bzip"; }
+        ];
+      };
+    };
+  };
+}
