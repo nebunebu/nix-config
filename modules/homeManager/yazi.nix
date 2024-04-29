4d882768fcc12fc6e95a387d8a1fb047e86356cf
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-04-28 20:21:38 -0400

    change yazi package to unstable yazi-unwrapped

--- modules/homeManager/yazi.nix
+++ modules/homeManager/yazi.nix
@@ -1,12 +1,31 @@
+{ inputs, unstablePkgs, ... }:
 {
   programs.yazi = {
     # FIX: Images not always in correct panel
     # FIX: Image remains when closed
     # FIX: Add nix-colors
     enable = true;
+    package = unstablePkgs.yazi-unwrapped;
     enableZshIntegration = true;
     # keymap = {};
-    # settings = {};
+    settings = {
+      log = {
+        enabled = false;
+      };
+      manager = {
+        show_hidden = false;
+      };
+      preview = {
+        max_width = 1920;
+        max_height = 1080;
+        image_filter = "triangel";
+      };
+      # opener = {
+      #   edit = {};
+      #   play = {};
+      #   open = {};
+      # };
+    };
     theme = {
       filetype = {
         rules = [
