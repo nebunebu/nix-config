ccf4ff873d5062a60007b73a066c95352b6a9cdd
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-05 14:39:05 -0400

    feat: add theme to sddm

--- modules/nixOS/sddm.nix
+++ modules/nixOS/sddm.nix
@@ -4,6 +4,12 @@
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      # theme = "${import /path/to/config}";
+      # services.xserver.displayManager.sddm.theme = fetchTarball {
+      #        url = "https://github.com/AlfredoRamos/urbanlifestyle-sddm-theme/archive/0.4.3.tar.gz";
+      #        sha256 = "1l1352in9djzj79mf3lr3w0m7miyg0my0ics7hpizgwqm1l7ppm2";
+      #    };
+      theme = "sddm-chili-theme";
     };
   };
 }
