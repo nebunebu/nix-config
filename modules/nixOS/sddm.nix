dc6cd5a30d55f3534b5183b25cdf238312f26b3a
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-06 13:54:24 -0400

    test(sddm): set sddm theme (broken)

--- modules/nixOS/sddm.nix
+++ modules/nixOS/sddm.nix
@@ -9,7 +9,7 @@
       #        url = "https://github.com/AlfredoRamos/urbanlifestyle-sddm-theme/archive/0.4.3.tar.gz";
       #        sha256 = "1l1352in9djzj79mf3lr3w0m7miyg0my0ics7hpizgwqm1l7ppm2";
       #    };
-      theme = "sddm-chili-theme";
+      # theme = "sddm-chili-theme";
     };
   };
 }
