bcc206818aa937eb170e99b1b54d307e1d774c4b
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-07 13:49:42 -0400

    feat(yazi): modify yazi

--- modules/homeManager/yazi.nix
+++ modules/homeManager/yazi.nix
@@ -1,13 +1,27 @@
-{ unstablePkgs, ... }:
+{ config, unstablePkgs, ... }:
+let
+  c = config.colorScheme.palette;
+  # c-gray = "${c.base07}";
+  # c-red = c.base08}";
+  # c-yellow = "#${c.base09}";
+  c-lteal = "#${c.base0C}";
+  # c-purple = "0xff${c.base0D}";
+in
 {
+  # xdg.configFile."yazi/plugins/smart-enter.yazi/init.lua" = /* lua */ ''
+  #   return {
+  #   	entry = function()
+  #   		local h = cx.active.current.hovered
+  #   		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+  #   	end,
+  #   }
+  # '';
+
   programs.yazi = {
-    # FIX: Images not always in correct panel
-    # FIX: Image remains when closed
     # FIX: Add nix-colors
     enable = true;
     package = unstablePkgs.yazi-unwrapped;
     enableZshIntegration = true;
-    # keymap = {};
     settings = {
       log = {
         enabled = false;
@@ -21,15 +35,17 @@
         image_filter = "triangel";
       };
       # opener = {
-      #   edit = {};
-      #   play = {};
-      #   open = {};
+      #   edit = {
+      #     run = "nvim $@";
+      #   };
+      #   play = { };
+      #   open = { };
       # };
     };
     theme = {
       filetype = {
         rules = [
-          { fg = "#7AD9E5"; mime = "image/*"; }
+          { fg = "${c-lteal}"; mime = "image/*"; }
           { fg = "#F3D398"; mime = "video/*"; }
           { fg = "#F3D398"; mime = "audio/*"; }
           { fg = "#CD9EFC"; mime = "application/x-bzip"; }
