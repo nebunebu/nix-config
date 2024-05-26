3fd26290e2d50c0de6346a68b3c6a31d0888cfc8
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-26 17:36:51 -0400

    feat(remotebuilder): add remotebuilder

--- hosts/t5610/nixOS/default.nix
+++ hosts/t5610/nixOS/default.nix
@@ -7,6 +7,29 @@
     inputs.home-manager.nixosModules.default
   ];
 
+  users.users.nixremote = {
+    name = "nixremote";
+    createHome = true;
+    group = "users";
+    shell = "/bin/sh";
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDohUrV/Gdhdzdz6GAc9aZqZWngsU02ERzENYFQSzO9A root@x230t"
+    ];
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "nixremote" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/nix-store";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
--- hosts/x230t/nixOS/default.nix
+++ hosts/x230t/nixOS/default.nix
@@ -5,27 +5,37 @@
     ./hardware-configuration.nix
     ../../../modules/nixOS
     inputs.home-manager.nixosModules.default
-    # inputs.niri.nixosModules.niri
   ];
 
-  # services = {
-  #   pipewire = {
-  #     enable = true;
-  #     wireplumber.enable = true;
-  #     audio.enable = true;
-  #     pulse.enable = true;
-  #     alsa = {
-  #       enable = true;
-  #       support32Bit = true;
-  #     };
-  #     jack.enable = true;
-  #   };
-  # };
+  nix = {
+    buildMachines = [{
+      hostName = "t5610";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 3;
+      speedFactor = 2;
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+      ];
+    }];
+
+    distributedBuilds = true;
+    extraOptions = ''
+      builders-use-substitutes = true
+    '';
+  };
+
+  programs.ssh.knownHosts = {
+    t5610 = {
+      hostNames = [ "t5610" ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBwBf5f3N2wZLx3nDsl5EUVRuLiYJpUeNasOWJzmymjr";
+    };
+  };
 
   desktop.hyprland.enable = true;
-  # programs.hyprland = {
-  #   enable = true;
-  # };
 
   networking.hostName = "x230t";
   environment = {
