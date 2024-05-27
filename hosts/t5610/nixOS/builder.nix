343c00a5982ffda77d1eb8768c23c4f1507b1028
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-27 14:06:13 -0400

    fix(deadnix): remove dead code

--- hosts/t5610/nixOS/builder.nix
+++ hosts/t5610/nixOS/builder.nix
@@ -1,19 +1,21 @@
 {
 
-  users.users.nixremote = {
+  users.users.builder = {
     name = "builder";
     createHome = true;
     group = "users";
     shell = "/bin/sh";
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDohUrV/Gdhdzdz6GAc9aZqZWngsU02ERzENYFQSzO9A root@x230t"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINS3YPeqVRXwWEGzFCBZcabEiwaETgCQ/91es0CQCM1E builder@t5610"
     ];
   };
 
+  nix.settings.trusted-users = [ "builder" ];
+
   security.sudo.extraRules = [
     {
-      users = [ "nixremote" ];
+      users = [ "builder" ];
       commands = [
         {
           command = "/run/current-system/sw/bin/nix-store";
