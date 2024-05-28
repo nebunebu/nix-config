fe69108d378f0dcef94ccb560c448be677551026
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-05-28 01:39:12 -0400

    fix(rotation): fix x230t screen rotation

--- modules/homeManager/hyprland/x230t/default.nix
+++ modules/homeManager/hyprland/x230t/default.nix
@@ -4,9 +4,11 @@ let
     pkgs.writeShellScript "hypr-tab" ''
       if [ "$(hyprctl monitors -j | jq '.[].transform')" = "0" ]; then
         hyprctl keyword input:touchdevice:transform 1
+        hyprctl keyword input:tablet:transform 1
         hyprctl keyword monitor LVDS-1, preferred, auto, 1, transform, 1
       else
         hyprctl keyword input:touchdevice:transform 0
+        hyprctl keyword input:tablet:transform 0
         hyprctl keyword monitor LVDS-1, preferred, auto, 1, transform, 0
       fi
     '';
