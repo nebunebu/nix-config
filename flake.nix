8f390d817b868b824f89f0dd2d3a08b45251466e
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-02 12:29:29 -0400

    chore(update): update nixos and hm release

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "nebu's NixOs configs";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
@@ -9,7 +9,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
