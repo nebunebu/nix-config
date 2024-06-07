2866d39512099f707626b5ee875f49b1dfb3e1cd
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-07 00:05:49 -0400

    add some binds for hyprgrass

--- modules/homeManager/hyprland/hyprgrass.nix
+++ modules/homeManager/hyprland/hyprgrass.nix
@@ -1,17 +1,33 @@
-{ inputs, pkgs, ... }:
+{ lib, inputs, config, pkgs, ... }:
+let
+  cfg = config.desktop.hyprland.hyprgrass;
+in
 {
-  wayland.windowManager.hyprland = {
-    plugins = [
-      inputs.hyprgrass.packages."${pkgs.system}".default
-    ];
+  options.desktop.hyprland.hyprgrass.enable = lib.mkEnableOption "enable hyprgrass";
 
-    extraConfig = /* hyprlang */ ''
-      plugin:touch_gestures {
-        sensitivity = 4.0
-        long_press_delay = 400
-        hyprgrass-bind = , edge:d:u, exec, firefox
-        hyprgrass-bind = , edge:u:d, exec, kitty
-      }
-    '';
+  config = lib.mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      plugins = [
+        inputs.hyprgrass.packages."${pkgs.system}".default
+      ];
+
+      extraConfig = /* hyprlang */ ''
+        plugin:touch_gestures {
+          sensitivity = 4.0
+          long_press_delay = 400
+          workspace_swipe_fingers = 3
+          long_press_delay = 400
+
+          hyprgrass-bind = , edge:d:u, exec, firefox
+          hyprgrass-bind = , edge:d:l, exec, fuzzel
+          hyprgrass-bind = , edge:u:d, exec, [float; noanim] kitty
+          hyprgrass-bind = , edge:r:l, workspace, +1
+          hyprgrass-bind = , edge:l:r, workspace, -1
+          # hyprgrass-bind = , tap:3, exec, kitty
+          hyprgrass-bindm = , longpress:2, movewindow
+          # hyprgrass-bind = , longpress:3, killactive
+        }
+      '';
+    };
   };
 }
