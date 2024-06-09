8fa1f118322bf01e1b31adf97273d83c3fef1780
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-09 18:11:29 -0400

    wrap wvkbd config in enable option

--- modules/homeManager/tablet/wvkbd.nix
+++ modules/homeManager/tablet/wvkbd.nix
@@ -1,32 +1,41 @@
-{ config, pkgs, unstablePkgs, ... }:
-
+{ lib, config, pkgs, unstablePkgs, ... }:
+let
+  cfg = config.tablet.wvkbd;
+in
 {
-  home.packages = [
-    unstablePkgs.wvkbd
-    pkgs.clickclack
-    # NOTE: not in nixpkgs
-    # https://git.sr.ht/~earboxer/swipeGuess
-    (pkgs.writeShellScriptBin "wvkbd-start" ''
-      wvkbd-mobintl\
-        -R 3\
-        --hidden\
-        --fn "${config.stylix.fonts.monospace.name}"\
-        --bg ${config.stylix.base16Scheme.palette.base00}\
-        --fg ${config.stylix.base16Scheme.palette.base0C}\
-        --fg-sp ${config.stylix.base16Scheme.palette.base0B}\
-        --press ${config.stylix.base16Scheme.palette.base05}\
-        --press-sp ${config.stylix.base16Scheme.palette.base04}\
-        --swipe ${config.stylix.base16Scheme.palette.base09}\
-        --swipe-sp ${config.stylix.base16Scheme.palette.base09}\
-        --text ${config.stylix.base16Scheme.palette.base02}\
-        --text-sp ${config.stylix.base16Scheme.palette.base05}\
-    '')
-  ];
+  options = {
+    tablet.wvkbd.enable = lib.mkEnableOption "enable wvkbd";
+  };
 
-  wayland.windowManager.hyprland.settings = {
-    exec-once = [ "wvkbd-start" ];
-    bind = [
-      ",XF86TaskPane, exec, pkill -SIGRTMIN wvkbd-mobintl"
+  config = lib.mkIf cfg.enable {
+
+    wayland.windowManager.hyprland.settings = {
+      exec-once = [ "wvkbd-start" ];
+      bind = [
+        ",XF86TaskPane, exec, pkill -SIGRTMIN wvkbd-mobintl"
+      ];
+    };
+
+    home.packages = [
+      unstablePkgs.wvkbd
+      pkgs.clickclack
+      # NOTE: not in nixpkgs
+      # https://git.sr.ht/~earboxer/swipeGuess
+      (pkgs.writeShellScriptBin "wvkbd-start" ''
+        wvkbd-mobintl\
+          -R 3\
+          --hidden\
+          --fn "${config.stylix.fonts.monospace.name}"\
+          --bg ${config.stylix.base16Scheme.palette.base00}\
+          --fg ${config.stylix.base16Scheme.palette.base0C}\
+          --fg-sp ${config.stylix.base16Scheme.palette.base0B}\
+          --press ${config.stylix.base16Scheme.palette.base05}\
+          --press-sp ${config.stylix.base16Scheme.palette.base04}\
+          --swipe ${config.stylix.base16Scheme.palette.base09}\
+          --swipe-sp ${config.stylix.base16Scheme.palette.base09}\
+          --text ${config.stylix.base16Scheme.palette.base02}\
+          --text-sp ${config.stylix.base16Scheme.palette.base05}\
+      '')
     ];
   };
 }
