58de06b2ba18bc357fa2967baee6e106a5efc049
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-09 22:11:09 -0400

    add xournal

--- custom/homeManager/xournalpp.nix
+++ custom/homeManager/xournalpp.nix
@@ -1,1 +1,16 @@
-{ }
+{ lib, config, pkgs, ... }:
+let
+  cfg = config.programs.xournalpp;
+in
+{
+  options.programs.xournalpp = {
+    enable = lib.mkEnableOption "enable xournalpp program";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      pkgs.xournalpp
+      pkgs.gnome.adwaita-icon-theme
+    ];
+  };
+}
