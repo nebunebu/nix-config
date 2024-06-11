40fb9c62ddbec3ef742bf9ff3f2f5ab04701d3b5
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-10 23:55:54 -0400

    add function for xournalpp.palette

--- custom/homeManager/xournalpp.nix
+++ custom/homeManager/xournalpp.nix
@@ -14,41 +14,52 @@ in
     # NOTE: use lib.generators.toINI
     # colornames = ;
 
-    # NOTE: use lib.generators.toINI
+    # TODO: set correct types
+    # There should be 11 colors
     palette = lib.mkOption {
-      type = lib.types.lines;
-      default = /* gpl */ ''
-        GIMP Palette
-        Name: Xournal Default Palette
-        #
-        0 0 0 Black
-        0 128 0 Green
-        0 192 255 Light Blue
-        0 255 0 Light Green
-        51 51 204 Blue
-        128 128 128 Gray
-        255 0 0 Red
-        255 0 255 Magenta
-        255 128 0 Orange
-        255 255 0 Yellow
-        255 255 255 White
-      '';
-      example = "
-        GIMP Palette
-        Name: Xournal Default Palette
-        #
-        0 0 0 Black
-        0 128 0 Green
-        0 192 255 Light Blue
-        0 255 0 Light Green
-        51 51 204 Blue
-        128 128 128 Gray
-        255 0 0 Red
-        255 0 255 Magenta
-        255 128 0 Orange
-        255 255 0 Yellow
-        255 255 255 White
-      ";
+      type = lib.types.anything;
+      default = {
+        "Black" = "000000";
+        "Green" = "008000";
+        "Light Blue" = "00C0FF";
+        "Light Green" = "00FF00";
+        "Blue" = "3333CC";
+        "Gray" = "808080";
+        "Red" = "FF0000";
+        "Magenta" = "FF00FF";
+        "Orange" = "FF8000";
+        "Yellow" = "FFFF00";
+        "White" = "FFFFFF";
+      };
+      # default = /* gpl */ ''
+      #   GIMP Palette
+      #   Name: Xournal Default Palette
+      #   #
+      #   0 0 0 Black
+      #   0 128 0 Green
+      #   0 192 255 Light Blue
+      #   0 255 0 Light Green
+      #   51 51 204 Blue
+      #   128 128 128 Gray
+      #   255 0 0 Red
+      #   255 0 255 Magenta
+      #   255 128 0 Orange
+      #   255 255 0 Yellow
+      #   255 255 255 White
+      # '';
+      example = {
+        "Black" = "000000";
+        "Green" = "008000";
+        "Light Blue" = "00C0FF";
+        "Light Green" = "00FF00";
+        "Blue" = "3333CC";
+        "Gray" = "808080";
+        "Red" = "FF0000";
+        "Magenta" = "FF00FF";
+        "Orange" = "FF8000";
+        "Yellow" = "FFFF00";
+        "White" = "FFFFFF";
+      };
       description = "color palette";
     };
 
@@ -68,8 +79,46 @@ in
       pkgs.gnome.adwaita-icon-theme
     ];
 
+    xdg.configFile."xournalpp/palette.gpl".text =
+      let
+        base16AttrSet = {
+          "0" = 0;
+          "1" = 1;
+          "2" = 2;
+          "3" = 3;
+          "4" = 4;
+          "5" = 5;
+          "6" = 6;
+          "7" = 7;
+          "8" = 8;
+          "9" = 9;
+          "a" = 10;
+          "A" = 10;
+          "b" = 11;
+          "B" = 11;
+          "c" = 12;
+          "C" = 12;
+          "d" = 13;
+          "D" = 13;
+          "e" = 14;
+          "E" = 14;
+          "f" = 15;
+          "F" = 15;
+        };
 
-    xdg.configFile."xournalpp/palette.gpl".text = lib.mkDefault cfg.palette;
+        hexCharList = x:
+          lib.stringToCharacters x;
+
+        rgbHelper = str: i: j:
+          (base16AttrSet.${lib.strings.elemAt (hexCharList str) i} * 16)
+          + (base16AttrSet.${lib.strings.elemAt (hexCharList str) j});
+
+        hexToRGB = x:
+          "${builtins.toString (rgbHelper x 0 1)} ${builtins.toString (rgbHelper x 2 3)} ${builtins.toString (rgbHelper x 4 5)}";
+
+        header = [ "GIMP Palette" "Name: Xournal Default Palette" "#" ];
+      in
+      lib.concatLines (header ++ (lib.attrsets.mapAttrsToList (n: v: "${hexToRGB v} ${n}") cfg.palette));
 
     xdg.configFile."xournalpp/pagetemplate.ini".text = lib.mkDefault cfg.pagetemplate;
   };
