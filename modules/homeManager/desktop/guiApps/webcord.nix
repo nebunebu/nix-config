0aed05e9b79e715c8112e2f2ad3b45410e2b4305
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-12 23:51:19 -0400

    modify vesktop

--- modules/homeManager/desktop/guiApps/webcord.nix
+++ modules/homeManager/desktop/guiApps/webcord.nix
@@ -3,9 +3,10 @@
 {
   home.packages = [
     # override icon?
-    unstablePkgs.arrpc
-    unstablePkgs.webcord
-    unstablePkgs.discover-overlay
+    # unstablePkgs.arrpc
+    # unstablePkgs.webcord
+    # unstablePkgs.discover-overlay
+    # unstablePkgs.armcord
 
     # CREDIT: https://github.com/raidenovich/OuterHeaven/blob/850546a45382efd76a1776a988e278dc4c7fac3b/modules/home-manager/gui/vesktop.nix#L4
     (unstablePkgs.vesktop.overrideAttrs {
@@ -13,7 +14,7 @@
       desktopItems = [
         (pkgs.makeDesktopItem {
           name = "vesktop";
-          desktopName = "Discord";
+          desktopName = "Vesktop";
           # exec = "vesktop %U";
           exec = "vesktop --enable-features=VaapiIgnoreDriverChecks,VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,UseMultiPlaneFormatForHardwareVideo";
           # still using vektop icon
