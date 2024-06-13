ae5e382fdf47dd2f6377e13c332172cf1b2cc7f3
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-12 23:02:47 -0400

    change home-manager to follow nixpkgs stable

--- modules/homeManager/desktop/guiApps/webcord.nix
+++ modules/homeManager/desktop/guiApps/webcord.nix
@@ -1,10 +1,29 @@
-{ unstablePkgs, ... }:
+{ lib, unstablePkgs, pkgs, ... }:
 
 {
   home.packages = [
-    unstablePkgs.vesktop
+    # override icon?
+    unstablePkgs.arrpc
     unstablePkgs.webcord
     unstablePkgs.discover-overlay
+
+    # CREDIT: https://github.com/raidenovich/OuterHeaven/blob/850546a45382efd76a1776a988e278dc4c7fac3b/modules/home-manager/gui/vesktop.nix#L4
+    (unstablePkgs.vesktop.overrideAttrs {
+      # withSystemVencord = true;
+      desktopItems = [
+        (pkgs.makeDesktopItem {
+          name = "vesktop";
+          desktopName = "Discord";
+          # exec = "vesktop %U";
+          exec = "vesktop --enable-features=VaapiIgnoreDriverChecks,VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,UseMultiPlaneFormatForHardwareVideo";
+          # still using vektop icon
+          icon = "discord";
+          startupWMClass = "VencordDesktop";
+          genericName = "Internet Messenger";
+          keywords = [ "discord" "vencord" "electron" "chat" ];
+          categories = [ "Network" "InstantMessaging" "Chat" ];
+        })
+      ];
+    })
   ];
-  services.arrpc.enable = true;
 }
