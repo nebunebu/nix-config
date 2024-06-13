a48a7cf2eebe93a944c63ac089be984f9c3575ef
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-13 17:45:31 -0400

    modifying vestktop

--- modules/homeManager/desktop/guiApps/webcord.nix
+++ modules/homeManager/desktop/guiApps/webcord.nix
@@ -1,4 +1,4 @@
-{ lib, unstablePkgs, pkgs, ... }:
+{ lib, config, unstablePkgs, pkgs, ... }:
 
 {
   home.packages = [
@@ -27,4 +27,62 @@
       ];
     })
   ];
+
+
+  xdg.configFile."vesktop/themes/custom.theme.css".text =
+    let
+      palette = config.stylix.base16Scheme.palette;
+    in
+      /* css */ ''
+      :root {
+          --base00: #${palette.base01}; /* Black */
+          --base01: #${palette.base02}; /* Bright Black */
+          --base02: #26233a; /* Grey */
+          --base03: #${palette.base04}; /* Brighter Grey */
+          --base04: #${palette.base07}; /* Bright Grey */
+          --base05: #${palette.base05}; /* White */
+          --base06: #e0def4; /* Brighter White */
+          --base07: #524f67; /* Bright White */
+          --base08: #eb6f92; /* Red */
+          --base09: #f6c177; /* Orange */
+          --base0A: #ebbcba; /* Yellow */
+          --base0B: #31748f; /* Green */
+          --base0C: #9ccfd8; /* Cyan */
+          --base0D: #c4a7e7; /* Blue */
+          --base0E: #f6c177; /* Purple */
+          --base0F: #524f67; /* Magenta */
+
+          --primary-630: var(--base00); /* Autocomplete background */
+          --primary-660: var(--base00); /* Search input background */
+      }
+
+      .theme-light, .theme-dark {
+          --search-popout-option-fade: none; /* Disable fade for search popout */
+          --bg-overlay-2: var(--base00); /* These 2 are needed for proper threads coloring */
+          --home-background: var(--base00);
+          --background-primary: var(--base00);
+          --background-secondary: var(--base01);
+          --background-secondary-alt: var(--base01);
+          --channeltextarea-background: var(--base01);
+          --background-tertiary: var(--base00);
+          --background-accent: var(--base0E);
+          --background-floating: var(--base01);
+          --background-modifier-hover: #1917244c; /* 30% of base00 */
+          --background-modifier-selected: var(--base00);
+          --text-normal: var(--base05);
+          --text-secondary: var(--base00);
+          --text-muted: var(--base03);
+          --text-link: var(--base0C);
+          --interactive-normal: var(--base05);
+          --interactive-hover: var(--base05);
+          --interactive-active: var(--base07);
+          --interactive-muted: var(--base03);
+          --channels-default: var(--base04);
+          --channel-icon: var(--base04);
+          --header-primary: var(--base06);
+          --header-secondary: var(--base03);
+          --scrollbar-thin-track: transparent;
+          --scrollbar-auto-track: transparent;
+      }
+    '';
 }
