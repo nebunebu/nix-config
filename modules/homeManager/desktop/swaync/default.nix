d96e17b4446f01185c8815a01acb152dc7d43ecf
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-18 17:46:49 -0400

    updating swaync

--- modules/homeManager/desktop/swaync/default.nix
+++ modules/homeManager/desktop/swaync/default.nix
@@ -1,8 +1,5 @@
-{ config, ... }:
-let
-  palette = config.stylix.base16Scheme.palette;
-in
 {
+  imports = [ ./style.nix ];
   services.swaync = {
     enable = true;
     # package = ;
@@ -17,31 +14,17 @@ in
       control-center-margin-bottom = 0;
       control-center-margin-right = 0;
       control-center-margin-left = 0;
+      widgets = [ "title" "dnd" "inhibitors" "label" "buttons-grid" "mpris" "volume" ];
+      mpris = {
+        image-size = 96;
+        image-radius = 7;
+        blur = false;
+      };
       notification-2fa-action = true;
       notification-inline-replies = false;
       notification-icon-size = 64;
       notification-body-image-height = 100;
       notification-body-image-width = 200;
     };
-    style = #css
-      ''
-        .notification-row {
-          outline: none;
-        }
-
-        .notification-row:focus,
-        .notification-row:hover {
-          background-color: #${palette.base0C};
-        }
-
-        .notification {
-          background-color: #${palette.base0D};
-          border-radius: 12px;
-          margin: 6px 12px;
-          box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.3), 0 1px 3px 1px rgba(0, 0, 0, 0.7),
-            0 2px 6px 2px rgba(0, 0, 0, 0.3);
-          padding: 0;
-        }
-      '';
   };
 }
