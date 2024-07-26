ffdac3200c06e4009f2ec81f41a480e1c79f03fc
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-07-25 20:53:17 -0400

    pin hyprland

--- modules/nixOS/users/nebu.nix
+++ modules/nixOS/users/nebu.nix
@@ -10,7 +10,11 @@
       name = "nebu";
       isNormalUser = true;
       shell = pkgs.zsh;
-      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      extraGroups = [
+        "wheel"
+        "video"
+        "render"
+      ];
       # initialPassword = "password";
       hashedPassword = "$y$j9T$BrkDt/ClWWO.T/9z5EHYM0$uzInWXBqPfgUZgdM0JWQuHV7aIT.857sysLoz872Hl3";
       # hashedPasswordFile = config.sops.secrets.nebu-password.path;
