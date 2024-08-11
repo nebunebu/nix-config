6f8968b28e2ea989e4f24399ec3cf0a07aacd0af
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-10 21:59:41 -0400

    add gh

--- modules/homeManager/development/git.nix
+++ modules/homeManager/development/git.nix
@@ -9,23 +9,36 @@ in
 
   config = lib.mkIf cfg.enable {
     # NOTE: def lot could be added for qol
-    programs.git = {
-      enable = true;
-      userEmail = "nebu.nebuchadnezzar@gmail.com";
-      userName = "nebunebu";
-      delta.enable = true;
-      extraConfig = {
-        init.defaultBranch = "main";
+    programs = {
+      git = {
+        enable = true;
+        userEmail = "nebu.nebuchadnezzar@gmail.com";
+        userName = "nebunebu";
+        delta.enable = true;
+        extraConfig = {
+          init.defaultBranch = "main";
+        };
+        aliases = {
+          st = "status";
+          ap = "add -p";
+          p = "push";
+          b = "branch";
+          sw = "switch";
+          # git rm --cached $(git ls-files --deleted)
+          rma = "rm --cached $(git ls-file --deleted)";
+        };
       };
-      aliases = {
-        st = "status";
-        ap = "add -p";
-        p = "push";
-        b = "branch";
-        sw = "switch";
-        # git rm --cached $(git ls-files --deleted)
-        rma = "rm --cached $(git ls-file --deleted)";
+      gh = {
+        enable = true;
+        settings = {
+          editor = "nvim";
+          git_protocol = "ssh";
+        };
       };
+      gh-dash = {
+        enable = true;
+      };
+
     };
   };
 }
