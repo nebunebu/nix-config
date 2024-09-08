7c5b13d68c95b1376f4502c89bdb110e00d8ff07
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-09-08 11:43:22 -0400

    remove comments from ./lib/utils.nix

--- lib/utils.nix
+++ lib/utils.nix
@@ -1,8 +1,6 @@
 { lib, pkgs, ... }:
 
 {
-  isEven = n: n lib.mod 2 == 0;
-
   # fromYAML = file:
   #   let
   #     json = pkgs.runCommand "converted.json" { } ''
@@ -14,51 +12,4 @@
   # TODO: add
   # mkHost
   # mapSystems
-
-  mkHost =
-    { hostName
-    , extraModules ? [ ]
-    , disableHomeManager ? false
-    ,
-    }:
-    inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = {
-        inherit
-          inputs
-          system
-          pkgs
-          unstablePkgs
-          ;
-        inherit (inputs) self;
-      };
-      modules =
-        [
-          ./hosts/${hostName}/nixOS/default.nix
-          inputs.stylix.nixosModules.stylix
-        ]
-        ++ extraModules
-        ++ (
-          if disableHomeManager then
-            [ ]
-          else
-            [
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.nebu = import ./hosts/${hostName}/homeManager/default.nix;
-                  extraSpecialArgs = {
-                    inherit
-                      inputs
-                      pkgs
-                      unstablePkgs
-                      ;
-                    inherit (inputs) self;
-                  };
-                };
-              }
-            ]
-        );
-    };
 }
