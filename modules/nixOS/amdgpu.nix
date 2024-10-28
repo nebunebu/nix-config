dc8d6a5ecda02a9fd88035c909cf78bdeaccfb74
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-10-27 21:49:44 -0400

    update amdgpu

--- modules/nixOS/amdgpu.nix
+++ modules/nixOS/amdgpu.nix
@@ -12,7 +12,10 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.lact ];
+    environment = {
+      systemPackages = [ pkgs.lact ];
+      ROC_ENABLE_PRE_VEGA = "1";
+    };
 
     systemd = {
       packages = [ pkgs.lact ];
@@ -24,6 +27,8 @@ in
         enable = true;
         driSupport = true;
         driSupport32Bit = true;
+        extraPackages = [ pkgs.rocmPackages.clr.icd pkgs.amdvlk ];
+        extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
       };
     };
 
