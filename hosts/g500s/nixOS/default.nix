c285fb7dee9b814243b74e001cc4c1aa72a874a0
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-11-03 22:34:30 -0500

    disable g500s, nixiso, strip back x230t

--- hosts/g500s/nixOS/default.nix
+++ hosts/g500s/nixOS/default.nix
@@ -1,35 +1,35 @@
 { inputs, pkgs, ... }:
 
 {
-  imports = [
-    inputs.home-manager.nixosModules.default
-    ../../../modules/nixOS/boot.nix
-    ../../../modules/nixOS/console.nix
-    ../../../modules/nixOS/documentation.nix
-    ../../../modules/nixOS/networking.nix
-    ../../../modules/nixOS/nix.nix
-    ../../../modules/nixOS/system.nix
-    ../../../modules/nixOS/users/nebu.nix
-    ../../../modules/nixOS/stylix.nix
-    ./hardware-configuration.nix
-    ./jellyfin.nix
-  ];
+  # imports = [
+  #   inputs.home-manager.nixosModules.default
+  #   ../../../modules/nixOS/boot.nix
+  #   ../../../modules/nixOS/console.nix
+  #   ../../../modules/nixOS/documentation.nix
+  #   ../../../modules/nixOS/networking.nix
+  #   ../../../modules/nixOS/nix.nix
+  #   ../../../modules/nixOS/system.nix
+  #   ../../../modules/nixOS/users/nebu.nix
+  #   ../../../modules/nixOS/stylix.nix
+  #   ./hardware-configuration.nix
+  #   ./jellyfin.nix
+  # ];
 
-  environment.systemPackages = [
-    pkgs.vbetool
-  ];
-
-  services = {
-    logind = {
-      lidSwitch = "ignore";
-      lidSwitchExternalPower = "ignore";
-    };
-  };
-
-  networking.hostName = "g500s";
-  environment = {
-    sessionVariables = {
-      HOSTNAME = "g500s";
-    };
-  };
+  # environment.systemPackages = [
+  #   pkgs.vbetool
+  # ];
+  #
+  # services = {
+  #   logind = {
+  #     lidSwitch = "ignore";
+  #     lidSwitchExternalPower = "ignore";
+  #   };
+  # };
+  #
+  # networking.hostName = "g500s";
+  # environment = {
+  #   sessionVariables = {
+  #     HOSTNAME = "g500s";
+  #   };
+  # };
 }
