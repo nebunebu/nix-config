10cf4a3573fc20837fadfc71733bb6cde0fdb926
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-11-22 13:43:16 -0500

    modify hyprland ui

--- hm-modules/desktop/window-managers/hyprland/default.nix
+++ hm-modules/desktop/window-managers/hyprland/default.nix
@@ -6,6 +6,14 @@
 }:
 let
   cfg = config.neb.desktop.window-managers.hyprland;
+  borderEdgeGradient = col: (
+    let
+      colorString = c: "rgb(${c}) ";
+      firstColor = lib.concatStrings (lib.replicate 5 (colorString col));
+      secondColor = lib.concatStrings (lib.replicate 5 (colorString config.stylix.base16Scheme.palette.base00));
+    in
+    firstColor + secondColor + "211deg"
+  );
 in
 {
   imports = [
@@ -60,7 +68,6 @@ in
       '';
 
       settings = {
-
         dwindle = {
           preserve_split = true;
         };
@@ -71,25 +78,47 @@ in
             col.shadow = null;
             shadow = {
               enabled = true;
-              range = 4;
-              render_power = 3;
-              color = "rgba(1a1a1aee)";
+              range = 1;
+              render_power = 4;
+              sharp = true;
+              color = "rgba(25, 23, 36, 1.0)"; # base
+              # color = "rgba(246, 193, 119, 1.0)"; # gold
+              # color_inactive = "rgba(49, 116, 143, 1.0)"; #pine
+              # color_inactive = borderEdgeGradient config.stylix.base16Scheme.palette.base0D;
+              ignore_window = false;
+              offset = "12 12";
             };
-            rounding = "10";
+
+            rounding = "0";
             "active_opacity" = "1";
             "inactive_opacity" = "1";
           } [ "col" ]);
 
         windowrulev2 = lib.mkForce [
-          "bordercolor rgb(${config.stylix.base16Scheme.palette.base0D}), floating:1"
-          "bordercolor rgb(${config.stylix.base16Scheme.palette.base0B}), fullscreen:1"
+          # base0D
+          "bordercolor ${borderEdgeGradient config.stylix.base16Scheme.palette.base0D}, floating:1"
+          "bordercolor ${borderEdgeGradient config.stylix.base16Scheme.palette.base0B}, fullscreen:1"
+          # "bordercolor ,${borderEdgeGradient config.stylix.base16Scheme.palette.base0D}, fullscreen:1"
         ];
 
         general = {
-          "gaps_in" = "7";
-          "gaps_out" = "8";
-          "border_size" = "2";
-          "col.active_border" = lib.mkForce "rgb(${config.stylix.base16Scheme.palette.base09})";
+          "gaps_in" = "12";
+          "gaps_out" = "15";
+          "border_size" = "4";
+          # "col.active_border" = lib.mkForce "rgb(${config.stylix.base16Scheme.palette.base09})  rgb(${config.stylix.base16Scheme.palette.base00}) 200deg;";
+          "col.active_border" = lib.mkForce (
+            "rgb(${config.stylix.base16Scheme.palette.base09}) " + # 1. Gold
+            "rgb(${config.stylix.base16Scheme.palette.base09}) " + # 2. Gold
+            "rgb(${config.stylix.base16Scheme.palette.base09}) " + # 3. Gold
+            "rgb(${config.stylix.base16Scheme.palette.base09}) " + # 4. Gold
+            "rgb(${config.stylix.base16Scheme.palette.base09}) " + # 5. Gold
+            "rgb(${config.stylix.base16Scheme.palette.base00}) " + # 6. Black
+            "rgb(${config.stylix.base16Scheme.palette.base00}) " + # 7. Black
+            "rgb(${config.stylix.base16Scheme.palette.base00}) " + # 8. Black
+            "rgb(${config.stylix.base16Scheme.palette.base00}) " + # 9. Black
+            "rgb(${config.stylix.base16Scheme.palette.base00}) " + # 10. Black
+            "211deg"
+          );
         };
         misc = {
           "disable_hyprland_logo" = "true";
