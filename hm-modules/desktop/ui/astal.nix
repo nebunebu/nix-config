c26a8c79d178c8164a8283424bf438e87f6e0197
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-12-06 10:31:29 -0500

    add battery and network to astal (tmp)

--- hm-modules/desktop/ui/astal.nix
+++ hm-modules/desktop/ui/astal.nix
@@ -54,6 +54,8 @@ in
           inputs.ags.packages.${pkgs.system}.mpris
           inputs.ags.packages.${pkgs.system}.tray
           inputs.ags.packages.${pkgs.system}.wireplumber
+          inputs.ags.packages.${pkgs.system}.network
+          inputs.ags.packages.${pkgs.system}.battery
         ];
       };
     })
