54df6e3284031a6da1a9503d05e03c42fdea9e52
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-12-11 18:24:15 -0500

    add gate to awscli

--- hm-modules/development/awscli.nix
+++ hm-modules/development/awscli.nix
@@ -1,12 +1,24 @@
+{ lib, config, ... }:
+
+let
+  cfg = config.neb.development.awscli;
+in
 {
-  programs.awscli = {
-    enable = true;
-    # credentials = {};
-    # settings = {
-    #   "default" = {
-    #     region = "us-east-1";
-    #     output = "json";
-    #   };
-    # };
+  options.neb.development.awscli = {
+    enable = lib.mkEnableOption "enable awsclie";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.awscli = {
+      enable = true;
+      # credentials = {};
+      # settings = {
+      #   "default" = {
+      #     region = "us-east-1";
+      #     output = "json";
+      #   };
+      # };
+    };
+
   };
 }
