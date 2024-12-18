57373a43dc3a0e5ce9836cf34e4a294ec53395df
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-12-18 08:07:30 -0500

    update gpg and pass

--- sops/pass.nix
+++ sops/pass.nix
@@ -1,22 +1,54 @@
-{ pkgs, config, ... }:
+{ lib
+, config
+, pkgs
+, ...
+}:
+let
+  cfg = config.neb.sops.pass;
+in
 {
-  home.packages = [
-    pkgs.dmenu-wayland
-    pkgs.passff-host
-    pkgs.passExtensions.pass-import
-    pkgs.passExtensions.pass-genphrase
-    # pkgs.passExtensions.pass-update
-    # pkgs.passExtensions.pass-tomb
-    # pkgs.passExtensions.pass-otp
-    # pkgs.passExtensions.pass-file
-    # pkgs.passExtensions.pass-checkup
-    # pkgs.passExtensions.pass-audit
-  ];
-  programs.password-store = {
-    enable = true;
-    settings = {
-      PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
-      # PASSWORD_STORE_KEY =  "neb";
+
+  options = {
+    neb.sops.pass = {
+      enable = lib.mkEnableOption "enable taskwarrior";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # sops = {
+    #   secrets = {
+    #   };
+    #   templates = {
+    #     "pass" = {
+    #       owner = "nebu";
+    #       path = "";
+    #       content = /* ini */ ''
+    #       '';
+    #     };
+    #   };
+    # };
+    home-manager.users.nebu = {
+      home.packages = [
+        pkgs.dmenu-wayland
+        pkgs.passff-host
+        pkgs.passExtensions.pass-import
+        pkgs.passExtensions.pass-genphrase
+        # pkgs.passExtensions.pass-update
+        # pkgs.passExtensions.pass-tomb
+        # pkgs.passExtensions.pass-otp
+        # pkgs.passExtensions.pass-file
+        # pkgs.passExtensions.pass-checkup
+        # pkgs.passExtensions.pass-audit
+      ];
+
+      programs.password-store = {
+        enable = true;
+        settings = {
+          # PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.password-store";
+          PASSWORD_STORE_DIR = "/home/nebu/.password-store";
+          # PASSWORD_STORE_KEY =  "neb";
+        };
+      };
     };
   };
 }
