dcec4c82bb18e79badfa6613d6de3bd3e241b7c5
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-12-31 18:29:50 -0500

    add beaverhabits

---
+++ nixos-modules/beaverhabits.nix
@@ -0,0 +1,43 @@
+{ lib
+, config
+, ...
+}:
+let
+  cfg = config.neb.beaverhabits;
+in
+{
+  options.neb.beaverhabits = {
+    enable = lib.mkEnableOption "enable beaverhabits";
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    systemd.tmpfiles.rules = [
+      "d /var/lib/beaverhabits 0750 nobody nogroup -"
+    ];
+
+    virtualisation.oci-containers.containers.beaverhabits = {
+      image = "daya0576/beaverhabits:latest";
+      autoStart = true;
+
+      ports = [ "8081:8080" ];
+      # user = "";
+
+      volumes = [ "/var/lib/beaverhabits:/app/.user" ];
+
+      user = "nobody:nogroup";
+
+      environment = {
+        HABITS_STORAGE = "USER_DISK";
+        FIRST_DAY_OF_WEEK = "0";
+        MAX_USER_COUNT = "1";
+        ENABLE_IOS_STANDALONE = "false";
+        INDEX_SHOW_HABIT_COUNT = "true";
+      };
+
+      # extraOptions = [ "--restart=unless-stopped" ];
+    };
+
+    # networking.firewall.allowedTCPPorts = [ 8080 ];
+  };
+}
