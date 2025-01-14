0bed5e146f59da5066ab18c13e4fd78be0047a5b
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-14 06:46:48 -0500

    enable pipe operator

--- nixos-modules/nix.nix
+++ nixos-modules/nix.nix
@@ -3,7 +3,7 @@
 {
   nix = {
     settings = {
-      experimental-features = [ "nix-command flakes" ];
+      experimental-features = [ "nix-command flakes pipe-operators" ];
       auto-optimise-store = true;
       allowed-users = [ "@wheel" ];
       trusted-users = [ "@wheel" ];
