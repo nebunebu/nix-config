60cb5258d77964f525d5a1423d609c4667cce82c
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-18 11:17:43 -0500

    fix fuzzle cliphist size

--- hm-modules/desktop/utilities/fuzzel/scripts/cliphist-menu.nix
+++ hm-modules/desktop/utilities/fuzzel/scripts/cliphist-menu.nix
@@ -2,7 +2,7 @@
 {
   home.packages = [
     (pkgs.writeShellScriptBin "cliphist-menu" ''
-      cliphist list | fuzzel -p "󰅌 " -d -l 30 -w 125 -P 15 | cliphist decode | wl-copy
+      cliphist list | fuzzel -p "󰅌 " -d -l 25 -w 70 -P 15 | cliphist decode | wl-copy
     '')
   ];
 }
