9131ea6d281dc1e24fa3fea89523c2202c17bbc3
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-18 06:33:02 -0500

    tweak hyprpanel

--- hm-modules/desktop/ui/hyprpanel/common.nix
+++ hm-modules/desktop/ui/hyprpanel/common.nix
@@ -17,12 +17,17 @@
       "theme.bar.buttons.workspaces.hover" = "#908caa";
       "theme.bar.buttons.workspaces.numbered_active_underline_color" = "#f6c177";
       "theme.bar.buttons.workspaces.occupied" = "#c4a7e7";
+      "theme.bar.buttons.y_margins" = "0.2rem";
+      "theme.bar.outer_spacing" = "0.2rem";
     };
 
     settings = {
       bar = {
         launcher.autoDetectIcon = true;
-        workspaces.showWsIcons = true;
+        workspaces = {
+          showWsIcons = true;
+          workspaces = 9;
+        };
         clock.format = "%a %b %d  %-I:%M:%S %p";
       };
       theme = {
@@ -32,8 +37,6 @@
         bar = {
           layer = "bottom";
           transparent = true;
-          # outer_spacing = "0.2rem";
-          # y_margins = "0.2rem";
         };
       };
       menus = {
@@ -45,14 +48,18 @@
           weather.unit = "imperial";
         };
         dashboard = {
-          # directories = {
-          #   left = {
-          #     directory1 = {
-          #       command = "bash -c \"ghostty -e yazi $HOME/Media/\"";
-          #       label = "Media";
-          #     };
-          #   };
-          # };
+          controls.enabled = false;
+          directories.enabled = false;
+          shortcuts = {
+            # enabled = false;
+            left = {
+              shortcut1 = {
+                command = "firefox";
+                icon = "";
+                tooltip = "Firefox";
+              };
+            };
+          };
         };
       };
       wallpaper = {
--- hm-modules/desktop/ui/hyprpanel/t5610.nix
+++ hm-modules/desktop/ui/hyprpanel/t5610.nix
@@ -1,8 +1,5 @@
 {
   programs.hyprpanel = {
-    # override = {
-    # };
-
     layout = {
       "bar.layouts" = {
         "0" = {
@@ -19,81 +16,7 @@
     };
 
     settings = {
-      theme = {
-        font = {
-          size = "1rem";
-        };
-      };
-      menus = {
-        dashboard = {
-          shortcuts = {
-            left = {
-              shortcut1 = {
-                command = "firefox";
-                icon = "";
-                tooltip = "Firefox";
-              };
-              shortcut2 = {
-                command = "firefox";
-                icon = "";
-                tooltip = "Firefox";
-              };
-              shortcut3 = {
-                command = "firefox";
-                icon = "";
-                tooltip = "Firefox";
-              };
-              shortcut4 = {
-                command = "firefox";
-                icon = "";
-                tooltip = "Firefox";
-              };
-            };
-            right = {
-              shortcut1 = {
-                command = "firefox";
-                icon = "";
-                tooltip = "Firefox";
-              };
-              shortcut3 = {
-                command = "firefox";
-                icon = "";
-                tooltip = "Firefox";
-              };
-            };
-          };
-          directories = {
-            left = {
-              directory1 = {
-                command = "bash -c \"xdg-open $HOME/Downloads/\"";
-                label = "󰉍 Downloads";
-              };
-              directory2 = {
-                command = "bash -c \"xdg-open $HOME/Downloads/\"";
-                label = "󰉍 Downloads";
-              };
-              directory3 = {
-                command = "bash -c \"xdg-open $HOME/Downloads/\"";
-                label = "󰉍 Downloads";
-              };
-            };
-            right = {
-              directory1 = {
-                command = "bash -c \"xdg-open $HOME/Downloads/\"";
-                label = "󰉍 Downloads";
-              };
-              directory2 = {
-                command = "bash -c \"xdg-open $HOME/Downloads/\"";
-                label = "󰉍 Downloads";
-              };
-              directory3 = {
-                command = "bash -c \"xdg-open $HOME/Downloads/\"";
-                label = "󰉍 Downloads";
-              };
-            };
-          };
-        };
-      };
+      theme.font.size = "1rem";
     };
   };
 }
