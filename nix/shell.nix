c9d192c94781cdb9c7d9c0adb1bc413f19f419fa
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-18 11:04:02 -0500

    add shellHook

--- nix/shell.nix
+++ nix/shell.nix
@@ -8,6 +8,21 @@ pkgs.mkShell {
     pkgs.deadnix
     pkgs.statix
   ];
+  shellHook =
+    let
+      cowWarn = pkgs.writeShellApplication {
+        name = "cowWarn";
+        runtimeInputs = [ pkgs.cowsay ];
+        text = ''
+          if [ "$(git branch --show-current)" = "main" ]; then
+              echo -e "\033[1;35m$(cowsay -f tux 'Hey! You are on main. Before making changes, git pull and make a new branch.')\033[0m"
+            fi
+        '';
+      };
+    in
+    ''
+      ${cowWarn}/bin/cowWarn
+    '';
   # inherit (checks.pre-commit-check) shellHook;
   # buildInputs = checks.pre-commit-check.enabledPackages;
 }
