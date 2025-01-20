6e747f2e72120c9b44b38d5b4e4160dae16753b4
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-20 10:42:39 -0500

    update ollama

--- nixos-modules/ollama.nix
+++ nixos-modules/ollama.nix
@@ -12,14 +12,21 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    nixpkgs.config.rocmSupport = true;
     services.ollama = {
       enable = true;
-      acceleration = "rocm";
+      # acceleration = "rocm";
       host = "0.0.0.0";
       loadModels = [ "qwen2.5:7b" "dolphin-mixtral:8x7b" ];
       openFirewall = true;
-      rocmOverrideGfx = "9.0.0";
+      # rocmOverrideGfx = "9.0.0";
+      # environmentVariables = {
+      #   ROCR_VISIBLE_DEVICES = "0";
+      #   ROC_ENABLE_PRE_VEGA = "1";
+      #   GPU_MAX_HEAP_SIZE = "100%";
+      #   GPU_USE_SYNC_OBJECTS = "1";
+      #   HSA_OVERRIDE_GFX_VERSION = "9.0.0";
+      #   HSA_ENABLE_SDMA = "0";
+      # };
     };
   };
 }
