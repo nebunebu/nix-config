9d45fc0e0cb45a5d02a1fa5e81f1d096c9768781
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-21 15:00:07 -0500

    config newsboat

--- hm-modules/terminal/media/newsboat/default.nix
+++ hm-modules/terminal/media/newsboat/default.nix
@@ -6,10 +6,6 @@ let
   cfg = config.neb.terminal.media.newsboat;
 in
 {
-  imports = [
-    ./scripts/linkhandler.nix
-  ];
-
   options = {
     neb.terminal.media.newsboat.enable = lib.mkEnableOption "enable newsboat";
   };
@@ -17,16 +13,59 @@ in
   config = lib.mkIf cfg.enable {
     programs.newsboat = {
       enable = true;
-      browser = "linkhandler";
       extraConfig = ''
-        urls-source "freshrss"
-        freshrss-url "http://192.168.1.177/api/greader.php"
-        freshrss-login "admin"
-        freshrss-flag-star "s"
-        freshrss-passworfile "~/.freshrss_key"
+              urls-source "freshrss"
+              freshrss-url "http://192.168.1.177/api/greader.php"
+              freshrss-login "admin"
+              freshrss-flag-star "b"
+              freshrss-passwordfile "~/.freshrss_key"
+
+              bind-key j down
+              bind-key k up
+              bind-key J next-feed articlelist
+              bind-key K prev-feed articlelist
+              bind-key G end
+              bind-key g home
+              bind-key d pagedown
+              bind-key u pageup
+              bind-key l open
+              bind-key h quit
+              bind-key a toggle-article-read
+              bind-key n next-unread
+              bind-key N prev-unread
+              bind-key U show-urls
+
+              # Base interface colors
+        color background          default   default
+        color listnormal          color8    default
+        color listfocus           color7    color0    bold
+        color listnormal_unread   color4    default   bold
+        color listfocus_unread    color4    color0    bold
+        color info                color5    color0    bold
+        color title               color5    color0    bold
+        color hint-key            color5    color0    bold
+        color hint-keys-delimiter color7    color0
+        color hint-separator      color7    color0    bold
+        color hint-description    color7    color0
+        color article             color7    default
+
+        # Article highlighting
+        highlight all "---.*---" color5
+        highlight feedlist ".*(0/0))" color8
+        highlight article "(^Feed:.*|^Title:.*|^Author:.*)" color6 default bold
+        highlight article "(^Link:.*|^Date:.*)" color7 default
+        highlight article "https?://[^ ]+" color4 default
+        highlight article "^(Title):.*$" color5 default
+        highlight article "\\[[0-9][0-9]*\\]" color6 default bold
+        highlight article "\\[image\\ [0-9]+\\]" color4 default bold
+        highlight article "\\[embedded flash: [0-9][0-9]*\\]" color4 default bold
+        highlight article ":.*\\(link\\)$" color6 default
+        highlight article ":.*\\(image\\)$" color5 default
+        highlight article ":.*\\(embedded flash\\)$" color6 default
+
       ''
-      + builtins.readFile ./keybinds.conf
-      + builtins.readFile ./colors.conf
+        # + builtins.readFile ./keybinds.conf
+        # + builtins.readFile ./colors.conf
       ;
     };
   };
