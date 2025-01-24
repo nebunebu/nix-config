280fa5abffff2a90e5459866f8703354da07c8c6
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-24 09:50:30 -0500

    fix nix-index

--- hm-modules/terminal/utilities/nix-index.nix
+++ hm-modules/terminal/utilities/nix-index.nix
@@ -25,10 +25,10 @@ in
         name = "download_nixpkgs_cache_index";
         runtimeInputs = [ pkgs.wget ];
         text = /* sh */ ''
-          filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
+          filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr '[:upper:]' '[:lower:]')"
           mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
-          wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
-          ln -f $filename files
+          wget -q -N "https://github.com/nix-community/nix-index-database/releases/latest/download/$filename"
+          ln -f "$filename" files
         '';
       })
     ];
