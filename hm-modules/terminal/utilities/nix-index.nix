960ead9bb41bf2041d857306ff117ed8f74926df
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-24 09:37:55 -0500

    fix nix-index

--- hm-modules/terminal/utilities/nix-index.nix
+++ hm-modules/terminal/utilities/nix-index.nix
@@ -12,26 +12,25 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home = {
-      programs = {
-        nix-index = {
-          enable = true;
-          enableZshIntegration = true;
-        };
+
+    programs = {
+      nix-index = {
+        enable = true;
+        enableZshIntegration = true;
       };
-      packages = [
-        pkgs.comma
-        (pkgs.writeShellApplication {
-          name = "download_nixpkgs_cache_index";
-          runtimeInputs = [ pkgs.wget ];
-          text = /* sh */ ''
-            filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
-            mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
-            wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
-            ln -f $filename files
-          '';
-        })
-      ];
     };
+    home.packages = [
+      pkgs.comma
+      (pkgs.writeShellApplication {
+        name = "download_nixpkgs_cache_index";
+        runtimeInputs = [ pkgs.wget ];
+        text = /* sh */ ''
+          filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
+          mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
+          wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
+          ln -f $filename files
+        '';
+      })
+    ];
   };
 }
