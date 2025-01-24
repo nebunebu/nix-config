509f4a9a5c58f93e40adc94d3c7afe3799a5a58d
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-24 10:13:40 -0500

    mv direnv

---
+++ hm-modules/terminal/utilities/direnv.nix
@@ -0,0 +1,20 @@
+{ inputs
+, lib
+, config
+, ...
+}:
+let
+  cfg = config.neb.terminal.utilities.nh;
+in
+{
+  options.neb.terminal.utilities.nh = {
+    enable = lib.mkEnableOption "enable nh";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.nh = {
+      enable = true;
+      flake = inputs.self;
+    };
+  };
+}
