7c1c042f162934b4b580e2b1e18763662586d591
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-25 15:52:38 -0500

    messing with pyradio

--- hm-modules/terminal/media/pyradio.nix
+++ hm-modules/terminal/media/pyradio.nix
@@ -14,13 +14,13 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.pyradio ];
 
-    xdg.configFile."pyradio/themes/rose-pine.pyradio.theme" =
-      let
-        theme = builtins.toFile (builtins.fetchurl {
-          url = "https://codeberg.org/thairanaru/pyradio/raw/branch/master/themes/rose-pine.pyradio-theme";
-          sha256 = "0hmyly8nzrgwcyk20rbglr632fm9bhzkr7nfbyns3g9qgznia3kc";
-        });
-      in
-      theme;
+    # xdg.configFile."pyradio/themes/rose-pine.pyradio.theme" =
+    #   let
+    #     theme = builtins.toFile (builtins.fetchurl {
+    #       url = "https://codeberg.org/thairanaru/pyradio/raw/branch/master/themes/rose-pine.pyradio-theme";
+    #       sha256 = "0hmyly8nzrgwcyk20rbglr632fm9bhzkr7nfbyns3g9qgznia3kc";
+    #     });
+    #   in
+    #   theme;
   };
 }
