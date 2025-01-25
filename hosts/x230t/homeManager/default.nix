3cf384d7071cb5a5c2fb15935537c0f4cc29387c
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-25 09:43:48 -0500

    minor changes

--- hosts/x230t/homeManager/default.nix
+++ hosts/x230t/homeManager/default.nix
@@ -20,28 +20,29 @@
     profile = {
       desktop = {
         enable = true;
-        # NOTE: not sure how I want to handle this
-        # x230t.enable = true;
       };
       development.enable = true;
       terminal.enable = true;
-      # NOTE: not set up
       # tablet.enable = false;
     };
 
+    # NOTE: not sure what I want to do with this
     desktop = {
       # ui.astal.x230t.enable = true;
       hyprpanel.x230t.enable = true;
 
-      window-managers.hyprland = {
-        enable = true;
-        x230t.enable = true;
-        hyprpaper.x230t.enable = true;
-        hyprpicker.enable = true;
-        pyprland = {
+      window-managers = {
+        # river.enable = true;
+        hyprland = {
           enable = true;
-          # btm.enable = true;
-          spotube.enable = true;
+          x230t.enable = true;
+          hyprpaper.x230t.enable = true;
+          hyprpicker.enable = true;
+          pyprland = {
+            enable = true;
+            # btm.enable = true;
+            spotube.enable = true;
+          };
         };
       };
     };
