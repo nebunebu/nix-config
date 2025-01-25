cb99735dfd48d99ab530952383868603279d26d7
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-25 08:30:32 -0500

    refactor desktop/browsers

--- hm-modules/desktop/browsers/default.nix
+++ hm-modules/desktop/browsers/default.nix
@@ -1,6 +1,24 @@
+{ lib
+, config
+, ...
+}:
+let
+  cfg = config.neb.desktop.browsers;
+in
 {
   imports = [
     ./chromium.nix
     ./firefox.nix
   ];
+
+  options.neb.desktop.browsers = {
+    enable = lib.mkEnableOption "enable browsers";
+  };
+
+  config = lib.mkIf cfg.enable {
+    neb.desktop.browsers = {
+      chromium.enable = true;
+      firefox.enable = true;
+    };
+  };
 }
