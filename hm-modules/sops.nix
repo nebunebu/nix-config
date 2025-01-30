372ddae31d54b23800a3c7f71842fdd7ec0257f0
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-01-30 13:28:24 -0500

    add sops to home-manager

--- hm-modules/sops.nix
+++ hm-modules/sops.nix
@@ -3,9 +3,13 @@
   sops = {
     defaultSopsFile = ../sops/secrets/secrets.yaml;
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
-    secrets.hm_test = {
-      path = "%r/hm_test.txt";
+    secrets = {
+      tuir_client_id.path = "%r/tuir_client_id.txt";
+      tuir_client_secret.path = "%r/tuir_client_secret.txt";
     };
+    # secrets.hm_test = {
+    #   path = "%r/hm_test.txt";
+    # };
   };
   # systemd.user.services."${some-service}".Unit.After = [ "sops-nix.service" ];
 }
