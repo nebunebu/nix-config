e3fee1bd10a875e0d5d75a159e5189bead5fcbfc
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-02-01 12:30:02 -0500

    remove rocm references

--- nixos-modules/ollama.nix
+++ nixos-modules/ollama.nix
@@ -1,6 +1,7 @@
-{ lib
-, config
-, ...
+{
+  lib,
+  config,
+  ...
 }:
 let
   cfg = config.neb.ollama;
@@ -14,19 +15,12 @@ in
   config = lib.mkIf cfg.enable {
     services.ollama = {
       enable = true;
-      # acceleration = "rocm";
       host = "0.0.0.0";
-      loadModels = [ "qwen2.5:7b" "dolphin-mixtral:8x7b" ];
+      loadModels = [
+        "qwen2.5:7b"
+        "dolphin-mixtral:8x7b"
+      ];
       openFirewall = true;
-      # rocmOverrideGfx = "9.0.0";
-      # environmentVariables = {
-      #   ROCR_VISIBLE_DEVICES = "0";
-      #   ROC_ENABLE_PRE_VEGA = "1";
-      #   GPU_MAX_HEAP_SIZE = "100%";
-      #   GPU_USE_SYNC_OBJECTS = "1";
-      #   HSA_OVERRIDE_GFX_VERSION = "9.0.0";
-      #   HSA_ENABLE_SDMA = "0";
-      # };
     };
   };
 }
