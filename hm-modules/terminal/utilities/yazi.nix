30c334e9b67176f38b4a299eb71421eaeaa2d60e
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-02-01 23:12:59 -0500

    change yazi package

--- hm-modules/terminal/utilities/yazi.nix
+++ hm-modules/terminal/utilities/yazi.nix
@@ -1,7 +1,8 @@
-{ lib
-, config
-, unstablePkgs
-, ...
+{
+  lib,
+  config,
+  unstablePkgs,
+  ...
 }:
 let
   cfg = config.neb.terminal.utilities.yazi;
@@ -12,30 +13,28 @@ in
     neb.terminal.utilities.yazi.enable = lib.mkEnableOption "enable yazi";
   };
 
-  config = lib.mkIf cfg.enable
-    {
-      programs.yazi = {
-        enable = true;
-        package = unstablePkgs.yazi-unwrapped;
-        enableZshIntegration = true;
-        settings = {
-          log = {
-            enabled = false;
-          };
-          manager = {
-            show_hidden = false;
-          };
-          preview = {
-            max_width = 1920;
-            max_height = 1080;
-            image_filter = "triangel";
-          };
+  config = lib.mkIf cfg.enable {
+    programs.yazi = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        log = {
+          enabled = false;
         };
-        theme = {
-          filetype = {
-            rules = [ ];
-          };
+        manager = {
+          show_hidden = false;
+        };
+        preview = {
+          max_width = 1920;
+          max_height = 1080;
+          image_filter = "triangel";
+        };
+      };
+      theme = {
+        filetype = {
+          rules = [ ];
         };
       };
     };
+  };
 }
