f595a6785b9b87c3761a0de3c6387ecc70eb68b0
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-02-05 03:33:50 -0500

    minor changes

--- nixos-modules/sddm.nix
+++ nixos-modules/sddm.nix
@@ -1,48 +1,51 @@
-{ lib
-, config
-, inputs
-, pkgs
-, ...
+{
+  lib,
+  config,
+  inputs,
+  pkgs,
+  ...
 }:
 let
   cfg = config.neb.desktop.sddm;
 in
 {
 
-  imports = [ inputs.sddm-sugar-candy-nix.nixosModules.default ];
+  imports = [
+    inputs.sddm-sugar-candy-nix.nixosModules.default
+    inputs.rosepine.nixosModules.rosePine
+  ];
 
   options = {
-    neb.desktop.sddm.enable = lib.mkEnableOption "enable fzf";
+    neb.desktop.sddm.enable = lib.mkEnableOption "enable sddm-sugar-candy-nix";
   };
 
-  config = lib.mkIf cfg.enable
-    {
-      environment.systemPackages = [ pkgs.libsForQt5.qt5.qtgraphicaleffects ];
-      services = {
-        displayManager.sddm = {
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.libsForQt5.qt5.qtgraphicaleffects ];
+    services = {
+      displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+        theme = "sddm-sugar-candy-nix";
+        sugarCandyNix = {
           enable = true;
-          wayland.enable = true;
-          theme = "sddm-sugar-candy-nix";
-          sugarCandyNix = {
-            enable = true;
-            settings = {
-              Background = builtins.fetchurl {
-                url = "https://github.com/nebunebu/imgs/raw/main/wallpapers/nixos-rose-pine.png";
-                sha256 = "084qgbicqiqwprb9m6l5jwij26rv8r73h76vba523z6q9rz0snlw";
-              };
-              HaveFormBackground = false;
-              HeaderText = "Welcome!";
-              FormPosition = "left";
-              PartialBlur = true;
-              MainColor = "#e0def4";
-              AccentColor = "#f6c177";
-              BackgroundColor = "#191724";
-              HourFormat = "hh:mm";
-              OverrideLoginButtonTextColor = "white";
-              Font = "IBM Plex";
+          settings = {
+            Background = builtins.fetchurl {
+              url = "https://github.com/nebunebu/imgs/raw/main/wallpapers/nixos-rose-pine.png";
+              sha256 = "084qgbicqiqwprb9m6l5jwij26rv8r73h76vba523z6q9rz0snlw";
             };
+            HaveFormBackground = false;
+            HeaderText = "Welcome!";
+            FormPosition = "left";
+            PartialBlur = true;
+            MainColor = "#${config.rosePine.main.text}";
+            AccentColor = "#${config.rosePine.main.gold}";
+            BackgroundColor = "#${config.rosePine.main.base}";
+            HourFormat = "hh:mm";
+            OverrideLoginButtonTextColor = "white";
+            Font = "IBM Plex";
           };
         };
       };
     };
+  };
 }
