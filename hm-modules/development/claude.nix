5388a407abe6317d35f9cf873821fe20e8674ed0
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-02-25 15:42:42 -0500

    add pkgs/claude-code

---
+++ hm-modules/development/claude.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  cfg = config.neb.development.claude;
+in
+{
+  options.neb.development.claude = {
+    enable = lib.mkEnableOption "enable claude";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      (pkgs.callPackage ../../pkgs/claude-code/package.nix { })
+    ];
+  };
+}
