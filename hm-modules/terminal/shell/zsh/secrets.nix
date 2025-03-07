8790f510724b3ce67d0b8749245c89916f9afd4c
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-03-06 19:02:24 -0500

    add gh api token

--- hm-modules/terminal/shell/zsh/secrets.nix
+++ hm-modules/terminal/shell/zsh/secrets.nix
@@ -7,6 +7,7 @@ in
     secrets = {
       anthropic_api_key.path = "%r/test.txt";
       gemini_api_key.path = "%r/test.txt";
+      gh_api.path = "%r/test.txt";
     };
     templates.api-keys = {
       name = "secrets";
@@ -16,6 +17,7 @@ in
         ''
           export CLAUDE_API_KEY=${config.sops.placeholder.anthropic_api_key}
           export GEMINI_API_KEY=${config.sops.placeholder.gemini_api_key}
+          export GH_TOKEN=${config.sops.placeholder.gh_api}
         '';
     };
   };
