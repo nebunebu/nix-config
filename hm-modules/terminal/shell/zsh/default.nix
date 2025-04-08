e1a19af7c3b43853639a13a7e9602e6190a16d20
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-04-08 12:49:46 -0400

    update manvim

--- hm-modules/terminal/shell/zsh/default.nix
+++ hm-modules/terminal/shell/zsh/default.nix
@@ -60,7 +60,7 @@ in
           ''
             # Function to use nvim as manpager
             function nman() {
-              man "$1" 2>/dev/null | sed 's/\x1B\[[0-9;]*m//g' | nvim -c 'set ft=man' -
+              man "$1" 2>/dev/null | sed 's/\x1B\[[0-9;]*m//g' | nix run "github:nebunebu/nebvim#manvim" -- -c 'set ft=man' -
             }
 
             function nfd() {
