464b8292d61d52dfe5d2b49b8e0b4b6e6dd0427f
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-04-12 17:50:14 -0400

    discover overlay

--- hm-modules/desktop/communications/default.nix
+++ hm-modules/desktop/communications/default.nix
@@ -11,7 +11,7 @@ in
   imports = [
     # ./vesktop.nix
     ./discord.nix
-    ./discover-overlay.nix
+    # ./discover-overlay.nix
     # ./element
     # ./telegram
     # something irc
@@ -22,10 +22,36 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+
+    home.packages = [
+    pkgs.discover-overlay
+    pkgs.discocss
+    ];
+
     neb.desktop.communications = {
-      # vesktop.enable = true;
       discord.enable = true;
-      # discover-overlay = true;
     };
+
+    # programs.discocss = {
+    #   enable = true;
+    #   discordAlias = true;
+    #   discordPackage = (pkgs.discord.overrideAttrs (_: rec {
+    #   version = "0.0.90";
+    #   src = pkgs.fetchurl {
+    #     url = "https://stable.dl2.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+    #     hash = "sha256-mfQo71G4Z5ZwDroWiI2W5Db5isMxdx4Fw3b6X+4n7Fg=";
+    #   };
+    #   }));
+    #   css = ''
+    #     .theme-light {
+    #       --background-primary: #f3f4f644;
+    #       --background-primary-alt: #eeeff244;
+    #       --background-secondary: #e5e7eb44;
+    #       --background-secondary-alt: #d1d5db44;
+    #       --background-tertiary: #dfe2e744;
+    #     }
+    #   '';
+    # };
+
   };
 }
