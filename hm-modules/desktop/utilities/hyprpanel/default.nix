31e6f73d2f02c21dad5e157b1495ebd777d32fdc
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-05-25 20:28:07 -0400

    update

--- hm-modules/desktop/utilities/hyprpanel/default.nix
+++ hm-modules/desktop/utilities/hyprpanel/default.nix
@@ -5,8 +5,8 @@
   ...
 }:
 let
-  # cfg = config.neb.desktop.hyprpanel;
-  # common = import ./common.nix;
+  cfg = config.neb.desktop.hyprpanel;
+  common = import ./common.nix;
   t5610 = import ./t5610.nix;
   x230t = import ./x230t.nix;
 in
@@ -22,8 +22,8 @@ in
 
   config = lib.mkMerge [
     # t5610
-    # (lib.mkIf cfg.t5610.enable common)
-    # (lib.mkIf cfg.t5610.enable t5610)
+    (lib.mkIf cfg.t5610.enable common)
+    (lib.mkIf cfg.t5610.enable t5610)
     # x230t
     # (lib.mkIf cfg.x230t.enable common)
     # (lib.mkIf cfg.x230t.enable x230t)
