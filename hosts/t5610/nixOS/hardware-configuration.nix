5763f478425102569932453cf53f84037f321149
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-06-15 21:47:31 -0400

    remove boot.intird.* from t5610

--- hosts/t5610/nixOS/hardware-configuration.nix
+++ hosts/t5610/nixOS/hardware-configuration.nix
@@ -6,25 +6,6 @@
 }:
 
 {
-  # imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-
-  # boot = {
-  #   initrd = {
-  #     availableKernelModules = [
-  #       "ata_generic"
-  #       "ehci_pci"
-  #       "ahci"
-  #       "xhci_pci"
-  #       "usb_storage"
-  #       "usbhid"
-  #       "sd_mod"
-  #     ];
-  #     kernelModules = [ "dm-snapshot" ];
-  #   };
-  #   kernelModules = [ "kvm-intel" ];
-  #   extraModulePackages = [ ];
-  # };
-
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-uuid/545637e7-7726-4f19-8c14-426a085a2535";
@@ -51,8 +32,4 @@
   };
 
   swapDevices = [ ];
-
-  # networking.useDHCP = lib.mkDefault true;
-  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
