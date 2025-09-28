74628d03beccb04436da6f050cf96af9975d4d62
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-09-28 04:08:12 -0400

    add thunar to t5610

--- hosts/t5610/nixos/default.nix
+++ hosts/t5610/nixos/default.nix
@@ -14,6 +14,16 @@
     ./builder.nix
   ];
 
+  programs.thunar = {
+    enable = true;
+    plugins = [
+      pkgs.xfce.thunar-archive-plugin
+      pkgs.xfce.thunar-volman
+      pkgs.xfce.thunar-media-tags-plugin
+      pkgs.xfce.thunar-vcs-plugin
+    ];
+  };
+
   services = {
     gvfs.enable = true;
     # NOTE: required for spotube
