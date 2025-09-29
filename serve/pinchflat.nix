a2b073be61c39e16aa223ceb8cf2bfb8a3aa6391
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-09-29 02:33:00 -0400

    update nixpkgs

--- serve/pinchflat.nix
+++ serve/pinchflat.nix
@@ -17,5 +17,22 @@ in
       openFirewall = true;
       selfhosted = true;
     };
+
+    # ensure dir exists with right perms
+    systemd.tmpfiles.rules = [
+      "d /var/lib/pinchflat/extras/yt-dlp-configs 0750 pinchflat pinchflat -"
+    ];
+
+    # stage the file from /etc, then copy pre-start
+    environment.etc."pinchflat/yt-dlp-base-config.txt".text = ''
+      --extractor-args youtube:player-client=default,-tv_simply
+    '';
+
+    systemd.services.pinchflat.preStart = ''
+      install -m0640 -o pinchflat -g pinchflat -D \
+        /etc/pinchflat/yt-dlp-base-config.txt \
+        /var/lib/pinchflat/extras/yt-dlp-configs/base-config.txt
+    '';
+
   };
 }
