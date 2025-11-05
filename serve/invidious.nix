0b4e68bc248b387ad9b7dbf028c196def9d522e1
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-05 11:48:19 -0500

    invidious issues

--- serve/invidious.nix
+++ serve/invidious.nix
@@ -12,14 +12,23 @@ in
     services = {
       invidious = {
         enable = true;
+        # database.createLocally = true;
         # extraSettingsFile = "";
-        address = "0.0.0.0";
-        port = "3013";
-        # settings = { };
+        #   address = "0.0.0.0";
+        #   port = "3013";
+        #   settings = {
+        #     settings = {
+        #       db = {
+        #         user = "invidious";
+        #         dbname = "invidious";
+        #       };
+        #     };
+        #   };
+        # };
+        # invidious-router = {
+        #   enable = true;
+        # };
       };
-      # invidious-router = {
-      #   enable = true;
-      # };
     };
   };
 }
