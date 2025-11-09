e1fcf84f0a3597d6e367c0d957ee0ea484b6d814
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-09 12:48:23 -0500

    update vesktop

--- hm-modules/desktop/communications/vesktop.nix
+++ hm-modules/desktop/communications/vesktop.nix
@@ -14,36 +14,33 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      # CREDIT: https://github.com/raidenovich/OuterHeaven/blob/850546a45382efd76a1776a988e278dc4c7fac3b/modules/home-manager/gui/vesktop.nix#L4
-
-      (
-        # (pkgs.vesktop.override {
-        #   electron = pkgs.electron_33;
-        # }).overrideAttrs
-        pkgs.vesktop.overrideAttrs (_: {
-          # withSystemVencord = true;
-          desktopItems = [
-            (pkgs.makeDesktopItem {
-              name = "vesktop";
-              desktopName = "Vesktop";
-              exec = "vesktop --disable-gpu-memory-buffer-video-frames --disable-gpu-compositing --enable-features=WebRTCPipeWireCapturer --ozone-platform-hint=auto --use-gl=egl";
-              startupWMClass = "VencordDesktop";
-              genericName = "Internet Messenger";
-              keywords = [
-                "discord"
-                "vencord"
-                "electron"
-                "chat"
-              ];
-              categories = [
-                "Network"
-                "InstantMessaging"
-                "Chat"
-              ];
-            })
-          ];
-        })
-      )
+      pkgs.vesktop
+      # # CREDIT: https://github.com/raidenovich/OuterHeaven/blob/850546a45382efd76a1776a988e278dc4c7fac3b/modules/home-manager/gui/vesktop.nix#L4
+      # (
+      #   pkgs.vesktop.overrideAttrs (_: {
+      #     # withSystemVencord = true;
+      #     desktopItems = [
+      #       (pkgs.makeDesktopItem {
+      #         name = "vesktop";
+      #         desktopName = "Vesktop";
+      #         exec = "vesktop --disable-gpu-memory-buffer-video-frames --disable-gpu-compositing --enable-features=WebRTCPipeWireCapturer --ozone-platform-hint=auto --use-gl=egl";
+      #         startupWMClass = "VencordDesktop";
+      #         genericName = "Internet Messenger";
+      #         keywords = [
+      #           "discord"
+      #           "vencord"
+      #           "electron"
+      #           "chat"
+      #         ];
+      #         categories = [
+      #           "Network"
+      #           "InstantMessaging"
+      #           "Chat"
+      #         ];
+      #       })
+      #     ];
+      #   })
+      # )
     ];
   };
 }
