b7aa411d8cc79654bb5bdf1e4a14c6d8f08e885d
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-09 12:48:36 -0500

    update vesktop

--- hm-modules/desktop/communications/vesktop.nix
+++ hm-modules/desktop/communications/vesktop.nix
@@ -13,34 +13,6 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [
-      pkgs.vesktop
-      # # CREDIT: https://github.com/raidenovich/OuterHeaven/blob/850546a45382efd76a1776a988e278dc4c7fac3b/modules/home-manager/gui/vesktop.nix#L4
-      # (
-      #   pkgs.vesktop.overrideAttrs (_: {
-      #     # withSystemVencord = true;
-      #     desktopItems = [
-      #       (pkgs.makeDesktopItem {
-      #         name = "vesktop";
-      #         desktopName = "Vesktop";
-      #         exec = "vesktop --disable-gpu-memory-buffer-video-frames --disable-gpu-compositing --enable-features=WebRTCPipeWireCapturer --ozone-platform-hint=auto --use-gl=egl";
-      #         startupWMClass = "VencordDesktop";
-      #         genericName = "Internet Messenger";
-      #         keywords = [
-      #           "discord"
-      #           "vencord"
-      #           "electron"
-      #           "chat"
-      #         ];
-      #         categories = [
-      #           "Network"
-      #           "InstantMessaging"
-      #           "Chat"
-      #         ];
-      #       })
-      #     ];
-      #   })
-      # )
-    ];
+    home.packages = [ pkgs.vesktop ];
   };
 }
