fe5983a3120182d49c61a92359b00c5d1dd33e6e
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-22 19:00:41 -0500

    fix formatting

--- nixos-modules/dms-greeter.nix
+++ nixos-modules/dms-greeter.nix
@@ -8,13 +8,13 @@ let
   cfg = config.nos.dms-greeter;
 in
 {
+  imports = [ inputs.dankMaterialShell.nixosModules.greeter ];
+
   options.nos.dms-greeter = {
     enable = lib.mkEnableOption "enable dms-greeter configuration";
   };
 
   config = lib.mkIf cfg.enable {
-    imports = [ inputs.dankMaterialShell.nixosModules.greeter ];
-
     programs.dankMaterialShell.greeter = {
       enable = true;
       compositor = {
