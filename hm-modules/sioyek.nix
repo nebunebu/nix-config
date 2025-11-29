434ce30187e3232f530bf23ee5ce2711090c53b4
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-11-29 12:41:54 -0500

    turn off dark mode

--- hm-modules/sioyek.nix
+++ hm-modules/sioyek.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 let
   cfg = config.hm.sioyek;
 in
@@ -8,6 +13,7 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    home.packages = [ pkgs.pdftk ];
     programs.sioyek = {
       enable = true;
       bindings = {
@@ -19,9 +25,12 @@ in
         "new_window" = "<C-t>";
       };
       config = {
-        "default_dark_mode" = "1";
+        "default_dark_mode" = "0";
         "dark_mode_background_color" = "#191724";
         "background_color" = "#1f1d2e";
+
+        # pdftk "$1" cat $2-$3 output "${1%.pdf}_pages_${2}-${3}.pdf"
+        # new_command _extract_pages /path/to/extract_pages.sh "%{file_path}" [start] [end]
       };
     };
   };
