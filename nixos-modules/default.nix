4ed73c6f7f8a1307500ece71a35ba662203254e4
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-12-27 15:19:17 -0500

    refactor

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -28,25 +28,12 @@
     inputs.nebvim.packages."${pkgs.stdenv.hostPlatform.system}".default
   ];
 
-  # Enable core system modules
-  # nos = {
-  #   avahi.enable = true;
-  #   boot.enable = true;
-  #   console.enable = true;
-  #   # dms-greeter.enable = true;
-  #   documentation.enable = true;
-  #   networking.enable = true;
-  #   nix.enable = true;
-  #   system.enable = true;
-  # };
-  #
   # qt.enable = true;
   # virtualisation.docker.enable = true;
   # nos.window-managers.mango.enable = true;
   #
   # environment = {
   #   systemPackages = [
-  #     inputs.nebvim.packages."${pkgs.stdenv.hostPlatform.system}".default
   #     inputs.direnv-instant.packages.${pkgs.stdenv.hostPlatform.system}.default
   #     inputs.anki.packages.${pkgs.stdenv.hostPlatform.system}.default
   #     pkgs.poppler-utils
