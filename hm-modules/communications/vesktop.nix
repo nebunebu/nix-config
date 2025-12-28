2ce8f9300b699ef3fb6ade0924384f053a7106a5
nebunebu <nebu.nebuchadnezzar@gmail.com>
2025-12-28 03:22:24 -0500

    try to fix vesktop streaming

--- hm-modules/communications/vesktop.nix
+++ hm-modules/communications/vesktop.nix
@@ -13,23 +13,15 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.vesktop ];
-    # home.packages = [
-    #   (pkgs.vesktop.overrideAttrs (_: {
-    #     postFixup = lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
-    #       makeWrapper ${pkgs.electron}/bin/electron $out/bin/vesktop \
-    #         --add-flags $out/opt/Vesktop/resources/app.asar \
-    #         --add-flags "--disable-gpu" \
-    #         --add-flags "--disable-gpu-compositing" \
-    #         --add-flags "--disable-software-rasterizer" \
-    #         ${lib.strings.optionalString pkgs.withTTS ''
-    #           --run 'if [[ "''${NIXOS_SPEECH:-default}" != "False" ]]; then NIXOS_SPEECH=True; else unset NIXOS_SPEECH; fi' \
-    #           --add-flags "\''${NIXOS_SPEECH:+--enable-speech-dispatcher}" \
-    #         ''} \
-    #         ${lib.optionalString pkgs.withMiddleClickScroll "--add-flags \"--enable-blink-features=MiddleClickAutoscroll\""} \
-    #         --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
-    #     '';
-    #   }))
-    # ];
+    home.packages = [
+      pkgs.vesktop
+
+      (pkgs.writeShellScriptBin "vesktop" ''
+        exec ${lib.getExe pkgs.vesktop} \
+          --disable-gpu-video-decode \
+          --disable-features=AcceleratedVideoDecodeLinuxGL,AcceleratedVideoDecoder,AcceleratedVideoDecodeLinuxZeroCopyGL \
+          "$@"
+      '')
+    ];
   };
 }
