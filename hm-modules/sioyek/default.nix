2eb1282d45697b6e28d97d988760095a73bb01da
nebunebu <nebu.nebuchadnezzar@gmail.com>
2026-01-09 06:04:09 -0500

    add dual_panelify

--- hm-modules/sioyek/default.nix
+++ hm-modules/sioyek/default.nix
@@ -6,6 +6,30 @@
 }:
 let
   cfg = config.hm.sioyek;
+  sioyekScripts = pkgs.python3Packages.buildPythonPackage {
+    pname = "sioyek-scripts";
+    version = "0.0.0";
+    src = ./.;
+    format = "other";
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/${pkgs.python3.sitePackages}/sioyek
+      cp ${./dual_panelify.py} $out/${pkgs.python3.sitePackages}/sioyek/dual_panelify.py
+      cp ${./extract_highlights.py} $out/${pkgs.python3.sitePackages}/sioyek/extract_highlights.py
+      cp ${./sioyek.py} $out/${pkgs.python3.sitePackages}/sioyek/sioyek.py
+      touch $out/${pkgs.python3.sitePackages}/sioyek/__init__.py
+    '';
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      numpy
+      pymupdf
+      pypdf2
+      pyqt5
+      regex
+    ];
+  };
+  pythonEnv = pkgs.python3.withPackages (_ps: [
+    sioyekScripts
+  ]);
 in
 {
   options = {
@@ -31,13 +55,14 @@ in
         "default_dark_mode" = "0";
         "dark_mode_background_color" = "#191724";
         "background_color" = "#1f1d2e";
+        "debug" = "1";
 
         "new_command _dual_panelify" = ''
-          python -m sioyek.dual_panelify "%{sioyek_path}" "%{file_path}" "%{command_text}"
+          ${pythonEnv}/bin/python -m sioyek.dual_panelify "%{sioyek_path}" "%{file_path}"
         '';
 
         "new_command _extract_highlights" = ''
-          python -m sioyek.extract_highlights "%{sioyek_path}" "%{local_database}" "%{shared_database}" "%1" %{zoom_level}
+          ${pythonEnv}/bin/python -m sioyek.extract_highlights "%{sioyek_path}" "%{local_database}" "%{shared_database}" "%{file_path}" %{zoom_level}
         '';
       };
     };
