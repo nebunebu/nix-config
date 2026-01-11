24f83d9aac3e9cf428fb094012ea2215c243950f
nebunebu <nebu.nebuchadnezzar@gmail.com>
2026-01-11 17:06:18 -0500

    enable checks

--- nix/checks.nix
+++ nix/checks.nix
@@ -14,20 +14,11 @@ inputs.nixpkgs.legacyPackages
     pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
       src = ./.;
       hooks = {
-        # Use treefmt for formatting
         treefmt = {
-          enable = false;
+          enable = true;
           package = inputs.self.formatter.${system};
         };
-
-        # Linting hooks
-        # statix.enable = true;
-
-        # Disabled hooks
-        nixpkgs-fmt.enable = false;
-        deadnix.enable = false; # Handled by treefmt
-        nil.enable = false;
-        # convco.enable = false; # User wants to wait on this
+        # convco.enable = false;
       };
     };
   }
