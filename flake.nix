eea1148fd62b1c426498b233c0392f66275b0e08
nebunebu <nebu.nebuchadnezzar@gmail.com>
2026-01-11 17:56:51 -0500

    remove dgop

--- flake.nix
+++ flake.nix
@@ -35,15 +35,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    dgop = {
-      url = "github:AvengeMedia/dgop";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        dgop.follows = "dgop";
       };
     };
 
