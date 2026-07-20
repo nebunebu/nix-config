61f8537a68a10913d0a79a68e89a9f326b97096f
nebunebu <nebu.nebuchadnezzar@gmail.com>
2026-07-20 17:03:30 -0400

    add codex

--- nixos-modules/nix.nix
+++ nixos-modules/nix.nix
@@ -29,7 +29,7 @@ in
         ];
 
         extra-trusted-public-keys = [
-          "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+          "cache.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
         ];
       };
       gc = {
